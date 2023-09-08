329359cef064e20635b570edbee4e71a59e4e37d
Richard Bignell <bignellrp@gmail.com>
2023-09-08 19:51:10 +0100

    Added login error

--- routes/login.py
+++ routes/login.py
@@ -12,11 +12,13 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
+        error = None
         if username == User("1").username and password == User("1").password:
             user = User("1")  # Create a User object
             login_user(user)  # Log in the user
             # Authentication successful, redirect to a protected page
             return redirect(url_for('index.index'))
+        else:
+            error = '*ERROR*: Invalid username or password!'
 
-    return render_template('login.html')
+    return render_template('login.html', error = error)
